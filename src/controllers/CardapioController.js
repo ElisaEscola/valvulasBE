b16df4e7dc4f607d46ffbe2432afe92a9d13b849
ElisaEscola <elisabeth.martins@escola.pr.gov.br>
2026-06-17 08:54:59 -0300

    pronto

--- src/controllers/CardapioController.js
+++ src/controllers/CardapioController.js
@@ -12,9 +12,21 @@ const listarCardapio = (req, res) => {
     });
 };
 
-const buscaPorId = (req,res) => {
+const buscaPorId = (req, res) => {
     const id = parseInt(req.perams.id)
     const itemEncontrado = cardapioOficial.find((item) => item.id === id)
+
+    return res.status(200).json({
+        erro: false,
+        sucesso: "Item foi encontrado, e esse valor é ",
+        item: itemEncontrado
+    })
+    if (!itemEncontrado) {
+        return res.status(400).json({
+            erro: "Erro 400!",
+            sucesso: false
+        })
+    }
 }
 
-module.exports = { listarCardapio };
+module.exports = { listarCardapio, buscaPorId };
